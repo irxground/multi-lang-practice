a608dcf0235326febabf7c5dc2d24702666af00a
irxground <irxnjhtchlnrw@gmail.com>
2013-09-30 16:51:18 +0900

    Implement D lang

--- src/main/d/MyMonad.d
+++ src/main/d/MyMonad.d
@@ -10,7 +10,7 @@ class List(T) {
     this.next = next;
   }
 
-  override const string toString() {
+  override const string toString() const {
     auto str = to!(string)(item) ~ ". ";
     if (next is null)
       return str;
@@ -25,63 +25,53 @@ class List(T) {
     return list;
   }
 
-  const U foldRight(U)(U seed, U delegate(T, U) f) {
+  U foldRight(U)(U seed, U delegate(T, U) f) const {
     if (next !is null)
       seed = next.foldRight(seed, f);
     return f(item, seed);
   }
 
-  const U foldLeft(U)(U seed, U delegate(U, T) f) {
+  const U foldLeft(U)(U seed, U delegate(U, T) f) const {
     auto v = f(seed, item);
     if (next is null)
       return v;
     return next.foldLeft(v, f);
   }
 
-  List!(T) opAdd(List!(T) that) {
-    return foldRight(that, (T item, List!(T) next) {
-      return new List!(T)(item, next);
-    });
+  List!(T) opAdd(List!(T) that) const {
+    return foldRight!(List!(T))(that, (item, next) => new List!(T)(item, next));
   }
 }
 
 /* ---- ---- ---- define Monadaa & use ---- ---- ---- */
 interface Monadaa(alias containerType) {
   alias containerType T;
-  static T!(B)  map(A, B)(T!(A) from, B delegate(A) f);
+  static T!(B) map(A, B)(T!(A) from, B delegate(A) f);
   static T!(B) bind(A, B)(T!(A) from, T!(B) delegate(A) f);
 }
 
 template UseMonadaa(alias M) {
-  M.T!(X) flat(A, B, X)(M.T!(A) xs, M.T!(B) ys, X delegate(A, B) f) {
-    return M.bind(xs, (A x) {
-      return M.map(ys, (B y) {
-        return f(x,y);
-      });
-    });
+  M.T!(X) flat(A, B, X)(M.T!(A) as, M.T!(B) bs, X delegate(A, B) f) {
+    return M.bind!(A, X)(as, a => M.map!(B, X)(bs, b => f(a, b)));
   }
 }
 
 /* ---- ---- ---- apply Monadaa ---- ---- ---- */
-
 class ListIsMonadaa : Monadaa!(List) {
-  static List!(B) map(A, B)(List!(A) from, B delegate(A) f) {
-    return null;
+  static List!(T2) map(T1, T2)(List!(T1) from, T2 delegate(T1) f) {
+    return from.foldRight!(List!(T2))(null, (item, l) => new List!(T2)(f(item), l));
   }
 
-  static List!(B) bind(A, B)(List!(A) from, List!(B) delegate(A) f) {
-    return null;
+  static List!(T2) bind(T1, T2)(List!(T1) from, List!(T2) delegate(T1) f) {
+    return from.foldRight!(List!(T2))(null, (item, l) => f(item) + l);
   }
 }
 
 /* ---- ---- ---- define List ---- ---- ---- */
-
 void main() {
-  auto one = List!(int).build( 1,  2,  3);
+  alias UseMonadaa!(ListIsMonadaa) mod;
+  auto one = List!(int).build(1, 2, 3);
   auto ten = List!(int).build(10, 20, 30);
-  auto plus = (int a, int b){ return a + b; };
-  writeln(one + ten);
-  auto flatten = UseMonadaa!(ListIsMonadaa).flat(one, ten, plus);
+  writeln(mod.flat!(int, int, int)(one, ten, (a, b) => a + b));
 }
-
 
